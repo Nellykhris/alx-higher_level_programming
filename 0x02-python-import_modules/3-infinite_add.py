b40f5279e683b31f0f16952327fed3db08a4134b
Nellykhris <ifeomaomeonu@gmail.com>
2022-06-30 23:28:46 +0100

    Python is cool

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    import sys
     add = 0
-    for s in argv[1:]:
+    for s in sys[1:]:
         add += int(s)
         print("{:d}".format(add))
