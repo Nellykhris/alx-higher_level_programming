eafe3422499d3d7dfe5e3492b4ddf7f8a6688ec2
Nellykhris <ifeomaomeonu@gmail.com>
2022-07-06 16:48:34 +0100

    Add special integers

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    sum = 0
-    my_list = list(set(my_list))
-    for i in my_list:
-        sum += i
-        return sum
+    return sum(set(my_list))
