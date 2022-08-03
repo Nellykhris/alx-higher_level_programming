fc8a99135c9f6de8626a3bbac3dff91e646e27b5
Nellykhris <ifeomaomeonu@gmail.com>
2022-08-03 15:25:41 +0100

    My commit

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,37 +1,17 @@
 #!/usr/bin/python3
-"""Module 12-student.
-Creates a Student class.
-"""
+"""Student class module"""
 
 
 class Student:
-    """Class that defines a student.
-    Public attributes:
-    - first_name
-    - last_name
-    - age
-    Public method to_json().
-    """
-
+    """Student class"""
     def __init__(self, first_name, last_name, age):
-        """Initializes the Student instance."""
-
+        """initializing the attributes"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation
-        of a Student instance.
-        Args:
-        - attrs: list of attribute
-        Returns: the dict representation of the instance.
-        """
-
-        my_dict = dict()
-        if type(attrs) is list and all(type(x) is str for x in attrs):
-            for x in attrs:
-                if x in self.__dict__:
-                    my_dict.update({x: self.__dict__[x]})
-                    return my_dict
-                return self.__dict__.copy()
+        """return the dictionary description of an object"""
+        if type(attrs) == list and all(type(ele) == str for ele in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.
