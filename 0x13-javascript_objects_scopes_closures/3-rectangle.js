3db9ca07aaa4b86173a64becc775484853abb17f
Nellykhris <ifeomaomeonu@gmail.com>
2022-10-05 18:03:08 +0100

    A claa rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,18 +4,18 @@
 class Rectangle {
   constructor (w, h) {
   // Method that initialize a instance
-     if (w > 0 && h > 0) {
-       this.width = w;
-       this.height = h;
-      }
- }
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
- print () {
- // Method that prints the rectangle using the charactere 'X'
-   for (let i = 0; i < this.height; i++) {
-     console.log('X'.repeat(this.width));
-   }
- }
+  print () {
+    // Method that prints the rectangle using the charactere 'X'
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
 }
 
 module.exports = Rectangle;
